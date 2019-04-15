c73bb2675333a602d48f8988b9fb1797b7f3ff48
Garry Fanata <garryfanata@gmail.com>
2019-04-14 22:58:53 -0700

    + added poster on cells

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -22,6 +23,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let synopsis = movie["overview"] as! String
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURL = URL(string: baseURL + posterPath)
+        
+        cell.posterView.af_setImage(withURL: posterURL!)
         
         return cell
     }
