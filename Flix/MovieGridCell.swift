b3574d85508e283fc8b59effbca9a94da59b364b
Garry Fanata <garryfanata@gmail.com>
2019-04-21 23:27:05 -0700

    + Superhero Grid Tab

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Garry Fanata on 4/21/19.
+//  Copyright © 2019 Garry Fanata. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet var posterView: UIImageView!
+    
+}
